b5bb2c0b216773207378222ba37ac76fc8e59c54
Dmitriy Yeryomin <Dmitriy.Yeryomin@itechart-group.com>
2019-08-28 17:27:56 +0300

    add insert test

--- gateway/gateway_test.go
+++ gateway/gateway_test.go
@@ -30,7 +30,7 @@ func TestShowList(t *testing.T) {
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			req, err := http.NewRequest(tc.method, "localhost:8000/", bytes.NewReader(tc.body))
-			assert.Error(t, err, fmt.Sprintf("couldn't create requset: %v", err))
+			assert.NoError(t, err, fmt.Sprintf("couldn't create requset: %v", err))
 
 			rec := httptest.NewRecorder()
 			gateway.ShowList(rec, req, mockCollecton)
@@ -39,7 +39,7 @@ func TestShowList(t *testing.T) {
 			defer res.Body.Close()
 
 			body, err := ioutil.ReadAll(res.Body)
-			assert.Error(t, err, fmt.Sprintf("couldn't read responce body: %v", err))
+			assert.NoError(t, err, fmt.Sprintf("couldn't read responce body: %v", err))
 
 			if tc.err != "" {
 				assert.Equal(t, http.StatusBadRequest, res.StatusCode, fmt.Sprintf("expected status Bad Request; got: %v", res.StatusCode))
@@ -47,19 +47,44 @@ func TestShowList(t *testing.T) {
 				return
 			}
 			assert.Equal(t, tc.status, res.StatusCode, fmt.Sprintf("expected status %v; got %v", tc.status, res.StatusCode))
-			assert.Error(t, json.Unmarshal(body, &expectedObject), err.Error())
+			assert.NoError(t, json.Unmarshal(body, &expectedObject), err.Error())
 		})
 	}
 }
 
-// for post test
-// var addedObject = &database.PersonalData{DocumentID: nil, Name: "firstName", LastName: "secondName", Phone: "", Email: "", YearOfBirth: 1980}
-//tt: {name: "post request", method: "POST", body: nil, status: http.StatusCreated},
-// if tc.method == "POST" {
-// 	var err error
-// 	tc.body, err = json.Marshal(addedObject)
-// 	if err != nil {
-// 		t.Fatalf("couldn't marshal request body: %v", err)
-// 	}
+func TestInsert(t *testing.T) {
+	var mockCollecton *database.Collection
+	tt := []struct {
+		name   string
+		method string
+		body   []byte
+		status int
+		err    string
+	}{
+		{name: "post request", method: "POST", body: nil, status: http.StatusCreated},
+	}
+	// addedObject which will used in request body.
+	addedObject := &database.PersonalData{DocumentID: nil, Name: "firstName", LastName: "secondName", Phone: "", Email: "", YearOfBirth: 1980}
 
-// }
+	for _, tc := range tt {
+		var err error
+		tc.body, err = json.Marshal(addedObject)
+		assert.NoError(t, err, fmt.Sprintf("couldn't marshal request body: %v", err))
+
+		req, err := http.NewRequest(tc.method, "localhost:8000/", bytes.NewReader(tc.body))
+		assert.NoError(t, err, fmt.Sprintf("couldn't create requset: %v", err))
+
+		rec := httptest.NewRecorder()
+		gateway.Insert(rec, req, mockCollecton)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		if tc.err != "" {
+			assert.Equal(t, http.StatusBadRequest, res.StatusCode, fmt.Sprintf("expected status Bad Request; got: %v", res.StatusCode))
+			//assert.Equal(t, tc.err, string(bytes.TrimSpace(body)), fmt.Sprintf("expected message %q; got %q", tc.err, string(bytes.TrimSpace(body))))
+			return
+		}
+		assert.Equal(t, tc.status, res.StatusCode, fmt.Sprintf("expected status %v; got %v", tc.status, res.StatusCode))
+	}
+}
