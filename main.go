efdb5998d4bc50adb55ac54bc9e327c0fbf999f5
Dmitriy Yeryomin <Dmitriy.Yeryomin@itechart-group.com>
2019-08-28 14:37:29 +0300

    connection db with comand line env

--- main.go
+++ main.go
@@ -1,40 +1,56 @@
 package main
 
 import (
-	"crud/database"
-	"crud/gateway"
+	"context"
+	"flag"
 	"log"
 	"net/http"
 
+	"github.com/EreminDm/golang_basic_crud/database"
+	"github.com/EreminDm/golang_basic_crud/gateway"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// init function to initializ db connection.
-func init() {
-	err := database.MongodbURIConnection()
-	if err != nil {
-		log.Fatal(`Couldn't connect to DB`)
-	}
-}
-
-// main function to init router.
 func main() {
+	// parsing command line flags.
+	conn, db := envf()
+
+	// create context for db connection.
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	defer ctxCancel()
+
+	// geting db collection.
+	collection, err := database.Connect(ctx, conn, db)
+	if err != nil {
+		log.Fatalf(`couldn't connect to database: %v`, err)
+	}
 
 	// port environment define to 8000.
-	err := http.ListenAndServe(":8000", handler())
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	log.Fatalf(`server initialization fail: %v`, http.ListenAndServe(":8000", handler(collection)))
 }
 
-func handler() http.Handler {
+// handler for routing map navigation.
+func handler(collection *mongo.Collection) http.Handler {
+	// making new router.
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", gateway.PersonalDatas).Methods("GET")
-	r.HandleFunc("/", gateway.PersonalDatas).Methods("POST")
-	r.HandleFunc("/{id}", gateway.PersonalDataByID).Methods("GET")
-	r.HandleFunc("/{id}", gateway.UpdatePersonalData).Methods("PUT")
-	r.HandleFunc("/{id}", gateway.RemovePersonalData).Methods("DELETE")
+	// handling urls API
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { gateway.ShowData(w, r, collection) }).Methods("GET")
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { gateway.Insert(w, r, collection) }).Methods("POST")
+	r.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) { gateway.PersonalDataByID(w, r, collection) }).Methods("GET")
+	r.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) { gateway.UpdatePersonalData(w, r, collection) }).Methods("PUT")
+	r.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) { gateway.RemovePersonalData(w, r, collection) }).Methods("DELETE")
 	return r
+}
+
+// envf reades command line flags for db connection,
+// connectURI flag returns db connection URI, example: http://localhost:5432 ,
+// databes flag returns db name.
+func envf() (string, string) {
+	var conn, db string
+	flag.StringVar(&conn, "connectURI", "http://uri:port", "a striong var")
+	flag.StringVar(&db, "database", "database_name", "a string var")
+	flag.Parse()
+	return conn, db
 }
