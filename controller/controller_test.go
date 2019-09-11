a7462cc06afc4cf9109c337f6a4854598ec71f21
Dmitriy Yeryomin <Dmitriy.Yeryomin@itechart-group.com>
2019-09-11 16:01:38 +0300

    controller 100% coverage tests

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -2,6 +2,7 @@ package controller_test
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -68,9 +69,183 @@ func TestInsert(t *testing.T) {
 	for _, tc := range tt {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			actual, err := tc.provider.Insert(tc.context, &tc.document)
+			_, err := tc.provider.Insert(tc.context, &tc.document)
 			assert.NoError(t, err, "could not insert data")
-			_, err = tc.provider.Remove(tc.context, actual.DocumentID)
+			_, err = tc.provider.Remove(tc.context, tc.document.DocumentID)
+			assert.NoError(t, err, "could not remove data")
+		})
+	}
+}
+
+func TestOne(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	var oid = primitive.NewObjectID().Hex()
+	m, err := mongo.Connect(ctx, "localhost:27017", "test")
+	if err != nil {
+		log.Fatalf(`couldn't connect to database: %v`, err)
+	}
+	// returns controller provider.
+	c := controller.New(m)
+
+	defer cancel()
+	tt := []struct {
+		name     string
+		provider controller.DBProvider
+		context  context.Context
+		document entity.PersonalData
+		expected entity.PersonalData
+	}{
+		{
+			name:     "One controller",
+			provider: c,
+			context:  ctx,
+			document: entity.PersonalData{
+				DocumentID:  oid,
+				Name:        "Name",
+				LastName:    "LName",
+				Phone:       "1235486",
+				Email:       "test@test.test",
+				YearOfBirth: 1234,
+			},
+			expected: entity.PersonalData{
+				DocumentID:  oid,
+				Name:        "Name",
+				LastName:    "LName",
+				Phone:       "1235486",
+				Email:       "test@test.test",
+				YearOfBirth: 1234,
+			},
+		},
+	}
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := tc.provider.Insert(tc.context, &tc.document)
+			assert.NoError(t, err, "could not insert data")
+			actual, err := tc.provider.One(tc.context, oid)
+			assert.NoError(t, err, "could not select data")
+			assert.Equal(t,
+				tc.expected,
+				actual,
+				fmt.Sprintf("expected object %v is not equals %v", tc.expected, actual),
+			)
+			_, err = tc.provider.Remove(tc.context, tc.document.DocumentID)
+			assert.NoError(t, err, "could not remove data")
+		})
+	}
+}
+
+func TestAll(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	var oid = primitive.NewObjectID().Hex()
+	m, err := mongo.Connect(ctx, "localhost:27017", "test")
+	if err != nil {
+		log.Fatalf(`couldn't connect to database: %v`, err)
+	}
+	// returns controller provider.
+	c := controller.New(m)
+
+	defer cancel()
+	tt := []struct {
+		name     string
+		provider controller.DBProvider
+		context  context.Context
+		document entity.PersonalData
+		expected []entity.PersonalData
+	}{
+		{
+			name:     "All controller",
+			provider: c,
+			context:  ctx,
+			document: entity.PersonalData{
+				DocumentID:  oid,
+				Name:        "Name",
+				LastName:    "LName",
+				Phone:       "1235486",
+				Email:       "test@test.test",
+				YearOfBirth: 1234,
+			},
+			expected: []entity.PersonalData{
+				{
+					DocumentID:  oid,
+					Name:        "Name",
+					LastName:    "LName",
+					Phone:       "1235486",
+					Email:       "test@test.test",
+					YearOfBirth: 1234,
+				},
+			},
+		},
+	}
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := tc.provider.Insert(tc.context, &tc.document)
+			assert.NoError(t, err, "could not insert data")
+			actual, err := tc.provider.All(tc.context)
+			assert.NoError(t, err, "could not select data")
+			assert.IsType(t,
+				tc.expected,
+				actual,
+				fmt.Sprintf("expected object %v is not equals %v", tc.expected, actual),
+			)
+			_, err = tc.provider.Remove(tc.context, tc.document.DocumentID)
+			assert.NoError(t, err, "could not remove data")
+		})
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	oid := primitive.NewObjectID().Hex()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	m, err := mongo.Connect(ctx, "localhost:27017", "test")
+	if err != nil {
+		log.Fatalf(`couldn't connect to database: %v`, err)
+	}
+	// returns controller provider.
+	c := controller.New(m)
+
+	defer cancel()
+	tt := []struct {
+		name         string
+		provider     controller.DBProvider
+		context      context.Context
+		document     entity.PersonalData
+		newDocument  entity.PersonalData
+		updatesCount int64
+	}{
+		{
+			name:     "Update controller",
+			provider: c,
+			context:  ctx,
+			document: entity.PersonalData{
+				DocumentID:  oid,
+				Name:        "Name",
+				LastName:    "LName",
+				Phone:       "1235486",
+				Email:       "test@test.test",
+				YearOfBirth: 1234,
+			},
+			newDocument: entity.PersonalData{
+				DocumentID:  oid,
+				Name:        "FirstName",
+				LastName:    "LastName",
+				Phone:       "1235486",
+				Email:       "test@test.test",
+				YearOfBirth: 1234,
+			},
+			updatesCount: 1,
+		},
+	}
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := tc.provider.Insert(tc.context, &tc.document)
+			assert.NoError(t, err, "could not insert data")
+			k, err := tc.provider.Update(tc.context, &tc.newDocument)
+			assert.NoError(t, err, "could not update data")
+			assert.Equal(t, tc.updatesCount, k, "not equals")
+			_, err = tc.provider.Remove(tc.context, tc.document.DocumentID)
 			assert.NoError(t, err, "could not remove data")
 		})
 	}
