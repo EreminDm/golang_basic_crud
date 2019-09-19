d38899426f2c44e7a8d45f32dc0f8841700df754
Dmitriy Yeryomin <Dmitriy.Yeryomin@itechart-group.com>
2019-09-19 16:01:11 +0300

    protobuf install fix

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
-tar -xzvf protobuf-2.4.1.tar.gz
-cd protobuf-2.4.1 && ./configure --prefix=/usr && make && sudo make install
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-all-3.6.1.tar.gz
+tar -xzvf protobuf-all-3.6.1.tar.gz
+pushd protobuf-3.6.1 && ./configure --prefix=/usr && make && sudo make install && popd
