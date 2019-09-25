0da23efd7d840b38266ae1564e0806166c22e1d5
Dmitriy Yeryomin <Dmitriy.Yeryomin@itechart-group.com>
2019-09-25 16:23:59 +0300

    root user

--- db/mariadb/migrations/baseline.sql
+++ db/mariadb/migrations/baseline.sql
@@ -1,16 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.8-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
@@ -33,13 +20,6 @@ CREATE TABLE IF NOT EXISTS `person` (
   `year_od_birth` year(4) DEFAULT NULL,
   KEY `Index 1` (`id`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
