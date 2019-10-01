bfd5cbd9cd35c00001d895f59394fff9a68fd09e
Dmitriy Yeryomin <Dmitriy.Yeryomin@itechart-group.com>
2019-10-01 10:56:32 +0300

    Add Docker file

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM golang:1.13-alpine
+WORKDIR 
+COPY . /crud
+RUN go-wrapper download
+RUN go-wrapper install
+CMD ["go-wrapper", "run", "-connectURI=localhost:27017", "-database_name=person", "-dbtype=mongo"]
+EXPOSE 8000 8888
--- install-protobuf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-all-3.6.1.tar.gz
-tar -xzvf protobuf-all-3.6.1.tar.gz
-pushd protobuf-3.6.1 && ./configure --prefix=/usr && make && sudo make install && popd
